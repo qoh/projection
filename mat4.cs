caa5b18315cc66c596c13967a4b7007d53f50052
Nick Smith <portification@gmail.com>
2015-04-08 10:03:18 +0200

    Implement inverse and rotate

--- mat4.cs
+++ mat4.cs
@@ -49,6 +49,49 @@ function mat4::mul_mat(%m, %n) {
 }
 
 function mat4::inverse(%m) {
+    %a00 = getWord(%a,  0); %a01 = getWord(%a,  1); %a02 = getWord(%a,  2); %a03 = getWord(%a,  3);
+    %a10 = getWord(%a,  4); %a11 = getWord(%a,  5); %a12 = getWord(%a,  6); %a13 = getWord(%a,  7);
+    %a20 = getWord(%a,  8); %a21 = getWord(%a,  9); %a22 = getWord(%a, 10); %a23 = getWord(%a, 11);
+    %a30 = getWord(%a, 12); %a31 = getWord(%a, 13); %a32 = getWord(%a, 14); %a33 = getWord(%a, 15);
+
+    %b00 = %a00 * %a11 - %a01 * %a10;
+    %b01 = %a00 * %a12 - %a02 * %a10;
+    %b02 = %a00 * %a13 - %a03 * %a10;
+    %b03 = %a01 * %a12 - %a02 * %a11;
+    %b04 = %a01 * %a13 - %a03 * %a11;
+    %b05 = %a02 * %a13 - %a03 * %a12;
+    %b06 = %a20 * %a31 - %a21 * %a30;
+    %b07 = %a20 * %a32 - %a22 * %a30;
+    %b08 = %a20 * %a33 - %a23 * %a30;
+    %b09 = %a21 * %a32 - %a22 * %a31;
+    %b10 = %a21 * %a33 - %a23 * %a31;
+    %b11 = %a22 * %a33 - %a23 * %a32;
+
+    %det = %b00 * %b11 - %b01 * %b10 + %b02 * %b09 + %b03 * %b08 - %b04 * %b07 + %b05 * %b06;
+    
+    if (%det == 0) {
+        return "";
+    }
+    
+    %det = 1 / %det;
+
+    return
+        (%a11 * %b11 - %a12 * %b10 + %a13 * %b09) * %det SPC
+        (%a02 * %b10 - %a01 * %b11 - %a03 * %b09) * %det SPC
+        (%a31 * %b05 - %a32 * %b04 + %a33 * %b03) * %det SPC
+        (%a22 * %b04 - %a21 * %b05 - %a23 * %b03) * %det SPC
+        (%a12 * %b08 - %a10 * %b11 - %a13 * %b07) * %det SPC
+        (%a00 * %b11 - %a02 * %b08 + %a03 * %b07) * %det SPC
+        (%a32 * %b02 - %a30 * %b05 - %a33 * %b01) * %det SPC
+        (%a20 * %b05 - %a22 * %b02 + %a23 * %b01) * %det SPC
+        (%a10 * %b10 - %a11 * %b08 + %a13 * %b06) * %det SPC
+        (%a01 * %b08 - %a00 * %b10 - %a03 * %b06) * %det SPC
+        (%a30 * %b04 - %a31 * %b02 + %a33 * %b00) * %det SPC
+        (%a21 * %b02 - %a20 * %b04 - %a23 * %b00) * %det SPC
+        (%a11 * %b07 - %a10 * %b09 - %a12 * %b06) * %det SPC
+        (%a00 * %b09 - %a01 * %b07 + %a02 * %b06) * %det SPC
+        (%a31 * %b01 - %a30 * %b03 - %a32 * %b00) * %det SPC
+        (%a20 * %b03 - %a21 * %b01 + %a22 * %b00) * %det;
 }
 
 function mat4::translate(%m, %v) {
@@ -63,7 +106,47 @@ function mat4::translate(%m, %v) {
         getWord(%a, 3) * %x + getWord(%a, 7) * %y + getWord(%a, 11) * %z + getWord(%a, 15);
 }
 
-function mat4::rotate(%m, %axis, %rads) {
+function mat4::rotate(%a, %axis, %rads) {
+    %x = getWord(%axis, 0);
+    %y = getWord(%axis, 1);
+    %z = getWord(%axis, 2);
+    %len = vectorLen(%axis);
+    
+    if (%len == 0) {
+        return %a;
+    }
+
+    %len = 1 / %len;
+    %x *= %len;
+    %y *= %len;
+    %z *= %len;
+
+    %s = mSin(%rads);
+    %c = mCos(%rads);
+    %t = 1 - %c;
+
+    %a00 = getWord(%a, 0); %a01 = getWord(%a, 1); %a02 = getWord(%a,  2); %a03 = getWord(%a,  3);
+    %a10 = getWord(%a, 4); %a11 = getWord(%a, 5); %a12 = getWord(%a,  6); %a13 = getWord(%a,  7);
+    %a20 = getWord(%a, 8); %a21 = getWord(%a, 9); %a22 = getWord(%a, 10); %a23 = getWord(%a, 11);
+
+    %b00 = %x * %x * %t + %c;      %b01 = %y * %x * %t + %z * %s; %b02 = %z * %x * %t - %y * %s;
+    %b10 = %x * %y * %t - %z * %s; %b11 = %y * %y * %t + %c;      %b12 = %z * %y * %t + %x * %s;
+    %b20 = %x * %z * %t + %y * %s; %b21 = %y * %z * %t - %x * %s; %b22 = %z * %z * %t + %c;
+
+    return
+        %a00 * %b00 + %a10 * %b01 + %a20 * %b02 SPC
+        %a01 * %b00 + %a11 * %b01 + %a21 * %b02 SPC
+        %a02 * %b00 + %a12 * %b01 + %a22 * %b02 SPC
+        %a03 * %b00 + %a13 * %b01 + %a23 * %b02 SPC
+        %a00 * %b10 + %a10 * %b11 + %a20 * %b12 SPC
+        %a01 * %b10 + %a11 * %b11 + %a21 * %b12 SPC
+        %a02 * %b10 + %a12 * %b11 + %a22 * %b12 SPC
+        %a03 * %b10 + %a13 * %b11 + %a23 * %b12 SPC
+        %a00 * %b20 + %a10 * %b21 + %a20 * %b22 SPC
+        %a01 * %b20 + %a11 * %b21 + %a21 * %b22 SPC
+        %a02 * %b20 + %a12 * %b21 + %a22 * %b22 SPC
+        %a03 * %b20 + %a13 * %b21 + %a23 * %b22 SPC
+        getWords(%a, 12, 15);
 }
 
 function mat4::project(%aspect, %field, %near, %far) {
